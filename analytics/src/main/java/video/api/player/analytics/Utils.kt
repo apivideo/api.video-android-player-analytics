0bbf25af594834ae9d39bf0dac5a65f53e0f2a8c
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-05-26 17:59:51 +0200

    feat(analytics): add support for custom domain

--- analytics/src/main/java/video/api/player/analytics/Utils.kt
+++ analytics/src/main/java/video/api/player/analytics/Utils.kt
@@ -3,6 +3,9 @@ package video.api.player.analytics
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
+import java.io.IOException
+import java.net.URL
+import java.util.regex.Pattern
 
 
 object Utils {
@@ -15,5 +18,50 @@ object Utils {
     fun nowUtcToIso(): String {
         val zdt = ZonedDateTime.now(ZoneId.of("UTC"))
         return zdt.format(DateTimeFormatter.ISO_INSTANT)
+    }
+
+    /**
+     * Parse a media URL such as `https://vod.api.video/vod/vi5oDagRVJBSKHxSiPux5rYD/hls/manifest.m3u8` to a [VideoInfo] object.
+     * @param mediaUrl the media URL to parse
+     * @param collectorDomainURL the collector domain URL
+     * @param vodDomainURL the vod domain URL
+     * @param liveDomainURL the live domain URL
+     */
+    fun parseMediaUrl(
+        mediaUrl: URL,
+        collectorDomainURL: URL,
+        vodDomainURLs: List<URL>,
+        liveDomainURL: URL
+    ): VideoInfo {
+        val regex =
+            "https:/.*[/].*/(?<id>(vi|li)[^/^.]*)[/.].*"
+        val pattern = Pattern.compile(regex)
+        val matcher = pattern.matcher(mediaUrl.toString())
+
+        if (matcher.groupCount() < 2) {
+            throw IOException("The media url doesn't look like an api.video URL.")
+        }
+
+        try {
+            matcher.find()
+            // Group naming is not supported before Android API 26
+            val videoType =
+                if (vodDomainURLs.any { mediaUrl.toString().startsWith(it.toString()) }) {
+                    VideoType.VOD
+                } else if (mediaUrl.toString().startsWith(liveDomainURL.toString())) {
+                    VideoType.LIVE
+                } else {
+                    throw IOException("The media url must start with ${vodDomainURLs.joinToString { ", " }} or $liveDomainURL")
+                }
+            val videoId = matcher.group(1) ?: throw IOException("Failed to get videoId")
+
+            return VideoInfo(
+                videoId,
+                videoType,
+                collectorDomainURL
+            )
+        } catch (e: Exception) {
+            throw IOException("The media url doesn't look like an api.video URL", e)
+        }
     }
 }
